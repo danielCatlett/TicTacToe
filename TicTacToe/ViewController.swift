b85590418e15730a21c13577ec1eeac3526db3aa
danielCatlett <daniel.catlett42@gmail.com>
2017-02-01 17:37:33 -0600

    Tic Tac Toe game

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -1,25 +1,45 @@
-//
 //  ViewController.swift
 //  TicTacToe
 //
 //  Created by Daniel Patrick Catlett on 2/1/17.
 //  Copyright © 2017 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    @IBOutlet weak var header: UILabel!
+    var count = 0
+    @IBAction func realButton(_ sender: UIButton)
+    {
+        if(count % 2 == 0)
+        {
+            sender.setTitle("X", for: .normal)
+        }
+        else
+        {
+            sender.setTitle("O", for: .normal)
+        }
+        
+        //checkIfGameWon()
+        
+        count += 1
+        
+        if(count == 8)
+        {
+            header.text = "Game Over"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /*
+    func checkIfGameWon()
+    {
+        if ((view.viewWithTag(0).titleLabel: UIButton) == (view.viewWithTag(0).titleLabel: UIButton)
+        {
+            header.text = "Game Over"
+        }
     }
-
+    */
 
 }
 
